41210cb1767158f16e1b8213cf16dc45758a083f
Aguscampu <agustinacampu@gmail.com>
2024-08-13 19:16:45 -0300

    Solución para evitar la propagación del evento click

--- evento.js
+++ evento.js
@@ -1,3 +1,8 @@
 document.getElementById('miDiv').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+
+document.querySelector('button').addEventListener('click', function(event) {
+    alert('Hola!');
+    event.stopPropagation(); // Detiene la propagación del evento click
+});
